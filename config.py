1e5bc940186645f5ff7ac2abe0ed2f3f8ae3eabf
Tom Kooij <tomkooij@tomkooij.nl>
2018-12-19 15:08:41 +0100

    Add MySQL configuration (pythonanywhere)
    TODO: Hide secrets in seperate file

--- config.py
+++ config.py
@@ -2,10 +2,28 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = 'you-will-never-guess'
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    DEBUG = True
+    FLASK_DEBUG = True
+
+    # local dhfdgr3xy757eufhfgfAGDSHRjhtr5456653wdvbmWESGnhgtutfhgfd43424tes
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    # pythonanywhere
+    MYSQLSERVER = 'tomkooij.mysql.pythonanywhere-services.com'
+    MYSQLDB = 'tomkooij$aanmeldr'
+    MYSQLUSER = 'tomkooij'
+    MYSQLPASS = 'fake'
+    #SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    #username=MYSQLUSER,
+    #password=MYSQLPASS,
+    #hostname=MYSQLSERVER,
+    #databasename=MYSQLDB,
+    #)
+
+    SQLALCHEMY_POOL_RECYCLE = 299
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     UPLOADED_DEFAULT_DEST = 'uploads'
     UPLOADED_DEFAULT_ALLOW = ('py', 'ipynb')
